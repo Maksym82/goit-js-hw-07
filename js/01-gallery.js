3f1b1edf914a128e0cafe59ab609ca68a2598971
Maksym <maksym.gromov82@gmail.com>
2023-06-01 15:48:18 +0300

    Создает разметку задача 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.addEventListener('click', onGalleryClick);
+let modalBox;
+const makeGalleryItems = ({ preview, original, description }) =>
+    `<li class = "gallery__item">
+        <a class = gallery__link" href= "${original}">
+            <img 
+                class = "gallery__image"
+                src = "${preview}"
+                data-source = "${original}"
+                alt = ${description}"
+            />
+        </a>
+    </li>`;
+
+const galleryList = galleryItems.map(el => makeGalleryItems(el)).join("");
+galleryContainer.insertAdjacentHTML('afterbegin', galleryList);
+
+function onGalleryClick(event) {
+    event.preventDefault();
+    if(event.target.nodeName !== 'IMG') {
+        return;
+    }
+    let originalImg = event.target.dataset.source;
+    modalBox = basicLightbox.create (
+        `<img width = "1400" height = "900" src = "${originalImg}">`,
+        {
+            onShow: () => {
+                document.addEventListener('keydown', onPressEsc);
+            },
+            onClose: () => {
+                document.removeEventListener('keydown', onPressEsc);
+            },
+        },
+    );
+    modalBox.show();
+}
+
+function onPressEsc(event) {
+    if (event.code === 'Escape' && modalBox != null) {
+        modalBox.close();
+    }
+}
